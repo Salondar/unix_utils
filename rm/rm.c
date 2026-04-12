fa69cbb7135edfe2f1186d2e69ae0900e6e8045c
Salondar <mrvllscbl41@gmail.com>
2026-04-12 18:19:49 +0200

    Full rewrite and also really bad

--- rm/rm.c
+++ rm/rm.c
@@ -1,33 +1,35 @@
 #include <stdio.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <err.h>
-#include <errno.h>
 #include <getopt.h>
-#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <err.h>
 #include <sys/stat.h>
-#include <sys/types.h>
+#include <dirent.h>
 #include <pwd.h>
 #include <grp.h>
-#include <dirent.h>
 #include <linux/limits.h>
-#include <string.h>
 
 int fflag = 0;
+int vflag = 0;
+int pflag = 0;
+int dflag = 0;
+int iflag = 0;
+int rflag = 0;
 
 void usage(void);
-int delete_folder(char *path);
-int delete_file(char *path);
+int remove_file(char *path);
+int remove_folder(char *path);
+int prompt(char *path, struct stat *st);
+int request_confirmation(char *path);
+int remove_hierarchy(char *path);
+int overwrite(char *path);
 
 int main(int argc, char **argv) {
-    int ch, vflag, dflag, iflag, rflag, pflag, rv = 0;
+    int ch, exit_status = 0;
     struct stat st;
-    
-    dflag = 0;
-    vflag = 0;
-    iflag = 0;
-    rflag = 0;
-    pflag = 0;
+
     while((ch = getopt(argc, argv, "dfiPRrv")) != -1) {
         switch(ch) {
             case 'd':
@@ -47,6 +49,7 @@ int main(int argc, char **argv) {
             case 'R':
             case 'r':
                 rflag = 1;
+                dflag = 0;
                 break;
             case 'v':
                 vflag = 1;
@@ -63,162 +66,209 @@ int main(int argc, char **argv) {
         if (fflag) {
             return 0;
         }
-        usage();
+        usage();    
     }
-   
-    for (; *argv != NULL; argv++) {
+    
+    
+    for (; *argv; argv++) {
+        if (strcmp(*argv, ".") == 0 || strcmp(*argv, "..") == 0) {
+            fprintf(stderr, "rm: \".\" and \"..\" may not be removed\n");
+            exit_status = 1;
+            continue;
+        }
+
+        if (strcmp(*argv, "/") == 0) {
+            fprintf(stderr, "rm: \"/\" may not be removed\n");
+            exit_status = 1;
+            continue;
+        }
+
+        if (rflag) {
+            exit_status  = remove_hierarchy(*argv);
+            continue;
+        }
         errno = 0;
+        if (stat(*argv, &st) == -1) {
+            if (!fflag) {
+                warn("%s", *argv);
+                exit_status = 1;
+            }
+            continue;
+        }
+        if (!(st.st_mode & S_IWUSR) && isatty(fileno(stdin)) && iflag == 0 && !(S_ISDIR(st.st_mode))) {
+            if (prompt(*argv, &st)) {
+               exit_status |= remove_file(*argv);
+                continue;
+            }
+        }
 
         if (dflag) {
-            if (rmdir(*argv) == -1) {
-                if (!fflag) {
-                    warn("%s", *argv);
-                    rv = 1;
+            if (!(st.st_mode & S_IWUSR) && isatty(fileno(stdin)) && iflag == 0) {
+                if (prompt(*argv, &st) == 0) {
+                    continue;
                 }
-               
             }
-        }
-        if (iflag) {
-            printf("remove %s: ", *argv);
-            if ((ch = getchar()) == 'y') {
-                rv = delete_file(*argv);
-            }
-            while ((ch = getchar()) != '\n' && ch != EOF);
-        }
-        if (vflag) {
-            rv = delete_file(*argv);
-            if (!rv) {
-                printf("%s\n", *argv);
-            }
-        }
-
-        if (pflag) {
-            FILE *fp = fopen(*argv, "r+");
-
-            if (!fp) {
-                fprintf(stderr, "Could not open file\n");
-            }
-            while((ch = fgetc(fp)) != EOF) {
-                fputc(arc4random(), fp);
-            }
-            fclose(fp);
-            delete_file(*argv);
-        }
-        if (rflag) {
-            stat(*argv, &st);
             if (S_ISDIR(st.st_mode)) {
-                rv = delete_folder(*argv);
+                exit_status |= remove_folder(*argv);
             } else {
-                rv = delete_file(*argv);
+                exit_status |= remove_file(*argv);
             }
+            continue;
         }
-        if (!(rflag | dflag | fflag | iflag | vflag | pflag))  {
-            int notwrite = 1;
-            __mode_t mode;
-            stat(*argv, &st);
-            struct passwd *pw = getpwuid(st.st_uid);
-            struct group *gr = getgrgid(st.st_gid);
-            mode = st.st_mode;
+        exit_status |= remove_file(*argv);
+    }
+    return exit_status;
+}
 
-            if (!(mode & S_IWUSR) && isatty(fileno(stdin))) {
-                printf("override %c", mode & S_IRUSR ? 'r' : '-');
-                printf("%c", mode & S_IWUSR ? 'w' : '-');
-                printf("%c", mode & S_IXUSR ? 'x' : '-');
+int prompt(char *path, struct stat *st) {
+    int rv = 0;
+    struct passwd *pw = getpwuid(st->st_uid); 
+    struct group *gr =  getgrgid(st->st_gid);
 
-                printf("%c", mode & S_IRGRP ? 'r' : '-');
-                printf("%c", mode & S_IWGRP ? 'w' : '-');
-                printf("%c", mode & S_IXGRP ? 'x' : '-');
+    printf("override %c", st->st_mode & S_IRUSR ? 'r' : '-');
+    printf("%c", st->st_mode & S_IWUSR ? 'w' : '-');
+    printf("%c", st->st_mode & S_IXUSR ? 'x' : '-');
 
-                printf("%c", mode & S_IROTH ? 'r' : '-');
-                printf("%c", mode & S_IWOTH ? 'w' : '-');
-                printf("%c", mode & S_IXOTH ? 'x' : '-');
+    printf("%c", st->st_mode & S_IRGRP ? 'r' : '-');
+    printf("%c", st->st_mode & S_IWGRP ? 'w' : '-');
+    printf("%c", st->st_mode & S_IXGRP ? 'x' : '-');
 
-                printf(" %s/%s for %s? ", pw->pw_name, gr->gr_name, *argv);
-
-                if ((ch = getchar()) == 'y') {
-                    delete_file(*argv);
-                }
-                while ((ch = getchar()) != '\n' && ch != EOF);
-                notwrite = 0;
-            }
-            if (notwrite) {
-                delete_file(*argv);       
-            }
-          
-        }
+    printf("%c", st->st_mode & S_IROTH ? 'r' : '-');
+    printf("%c", st->st_mode & S_IWOTH ? 'w' : '-');
+    printf("%c", st->st_mode & S_IXOTH ? 'x' : '-');
+    printf(" %s/%s for %s? ", pw->pw_name, gr->gr_name, path);
+    
+    if (request_confirmation(path)) {
+        rv = 1;
     }
     return rv;
 }
 
-int delete_file(char *path) {
+int request_confirmation(char *path) {
+    int ch, rv = 0;
+
+    if (iflag) {
+        printf("remove %s? ", path);
+    }
+
+    if ((ch = getchar()) == 'y') {
+        rv = 1;
+    }
+    while((ch = getchar()) != '\n');
+    return rv;
+}
+
+int remove_file(char *path) {
     errno = 0;
-    if (unlink(path) == -1) {
-        if (!fflag) {
-            warn("%s", path);
-            return 1;
-        }
+
+    if (iflag && request_confirmation(path) == 0) {
+        return 0;
+    }
+
+    if (unlink(path) != 0) {
+        warn("%s", path);
+        return 1;
+    }
+
+    if (vflag) {
+        printf("%s\n", path);
     }
     return 0;
 }
 
-int delete_folder(char *path) {
+int remove_folder(char *path) {
+    int rv = 0;
+
+    if (iflag && request_confirmation(path) == 0) {
+        return 0;
+    }
+
+    errno = 0;
+    if (rmdir(path) == -1) {
+        warn("%s", path);
+        rv = 1;
+    }
+
+    if (vflag) {
+        printf("%s\n", path);
+    }
+    return rv;
+}
+
+
+int remove_hierarchy(char *path) {
     struct stat st;
-    DIR *directory;
-    struct dirent *entry;
+    DIR *dir;
+    struct dirent *direntry;
     char filepath[PATH_MAX];
-    int rv;
+    int rv = 0;
     
     errno = 0;
-    if ((directory = opendir(path)) == NULL) {
-        __mode_t mode;
-        int opendir_errno = errno;
-        stat(path, &st);
-        struct passwd *pw = getpwuid(st.st_uid);
-        struct group *gr = getgrgid(st.st_gid);
-        mode = st.st_mode;
-        printf("override %c", mode & S_IRUSR ? 'r' : '-');
-        printf("%c", mode & S_IWUSR ? 'w' : '-');
-        printf("%c", mode & S_IXUSR ? 'x' : '-');
-
-        printf("%c", mode & S_IRGRP ? 'r' : '-');
-        printf("%c", mode & S_IWGRP ? 'w' : '-');
-        printf("%c", mode & S_IXGRP ? 'x' : '-');
-
-        printf("%c", mode & S_IROTH ? 'r' : '-');
-        printf("%c", mode & S_IWOTH ? 'w' : '-');
-        printf("%c", mode & S_IXOTH ? 'x' : '-');
-
-        printf(" %s/%s for %s? ", pw->pw_name, gr->gr_name, path);
-
-        if (getchar() == 'y') {
-            errno = opendir_errno;
-            warn("%s", path);
-        }
-        while (getchar() != '\n');
+    if ((dir = opendir(path)) == NULL) {
+        prompt(path, &st);
+        return 1;
     }
-    
-    while ((entry = readdir(directory)) != NULL) {
-        if ( strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            snprintf(filepath, PATH_MAX, "%s/%s", path, entry->d_name);
-            stat(filepath, &st);
+
+    while ((direntry = readdir(dir)) != NULL) {
+        if (strcmp(direntry->d_name, ".") != 0 && strcmp(direntry->d_name, "..") != 0) {
+            snprintf(filepath, PATH_MAX, "%s/%s", path, direntry->d_name);
+            errno = 0;
+            if (stat(filepath, &st) == -1) {
+                warn("%s", filepath);
+                return 1;
+            }
             if (S_ISDIR(st.st_mode)) {
-                rv = delete_folder(filepath);
+                rv = remove_hierarchy(filepath);
             } else {
-                rv = delete_file(filepath);
-            } 
+                remove_file(filepath);
+            }
         }
-      
     }
-    if (rmdir(path) == -1) {
+    rv = remove_folder(path);
+    return rv;
+}
+
+int overwrite(char *path) {
+    FILE *fp;
+    struct stat st;
+    int rv = 0;
+
+    errno = 0;
+    if (stat(path, &st) == -1) {
         if (!fflag) {
             warn("%s", path);
             rv = 1;
         }
     }
+
+    if (st.st_nlink == 1) {
+        if (S_ISREG(st.st_mode) && (st.st_mode & S_IWUSR)) {
+            errno = 0;
+            fp = fopen(path, "r+");
+            if (fp == NULL) {
+                warn("%s", path);
+                rv = 1;
+            }
+            int ch;
+            while((ch = fgetc(fp)) != EOF) {
+                fputc(arc4random(), fp);
+            }
+        }
+        
+    } else {
+        fprintf(stderr, "not overwritten due to multiple links");
+        rv = 1;
+    }
+
+    if (S_ISDIR(st.st_mode) && dflag) {
+        rv = remove_folder(path);
+    } else {
+        rv = remove_file(path);
+    }
     return rv;
 }
 
 void usage(void) {
-    fprintf(stderr, "usage: ./rm [-dfiPRrv] file ...\n");
+    fprintf(stderr, "./rm [-dfiPRrv] file ...\n");
     exit(EXIT_FAILURE);
 }
